e29de504e527dbe709a285ef351677f120eda1e7
Rohit Agrawal <rohitagrawalcgartist@gmail.com>
2019-06-10 19:07:42 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# twitter-sentiment-analysis-web-app
---
+++ main_app/config.py
@@ -0,0 +1,4 @@
+consumer_key = 'qhwg88DbtCpCG2hQumqSKj3qp'
+consumer_secret = 'BZy237443Jj7hePJvSnRUFMePKCnrXVtEbhzXYQbDEwtUm8LQy'
+access_token = '3315982002-w8V3IgrJWXKjNHuKjqMWOj7dGsTqG2rZaQSl91Y'
+access_token_secret = 'LBkNkwRhZga9O3MokOIClPagFWGBx97DDo6RXvWFwqjrv'
