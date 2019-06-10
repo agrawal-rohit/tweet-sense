fda7f9a483df59e0ee9da164ac051a02f95fb5ce
Rohit Agrawal <rohitagrawalcgartist@gmail.com>
2019-06-10 19:38:05 +0530

    more thingies

---
+++ react-frontend/build/precache-manifest.27ca9dbcc4b2e27f99e152b63af4ab8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b10ab5d868dec8b016e7f78c1c7a5d2a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "538a873770f105c59926",
+    "url": "./static/css/main.f538f147.chunk.css"
+  },
+  {
+    "revision": "638d4c6deeca5e5864e7",
+    "url": "./static/js/2.3c237e2d.chunk.js"
+  },
+  {
+    "revision": "538a873770f105c59926",
+    "url": "./static/js/main.2ccd0060.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "25ef280441ad6d3a5ccf89960b4e95eb",
+    "url": "./static/media/loading_bar.25ef2804.gif"
+  }
+]);
